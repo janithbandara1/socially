5f459128527cfcb2c0938d45865030037ff2ded2
janithbandara2 <janithbandara002@gmail.com>
2025-03-07 14:16:47 +0000

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000", // localhost
+        "localhost:3001",
+        "musical-guide-r5xv54x5r7g3v5x-3000.app.github.dev", // Codespaces
+        "musical-guide-r5xv54x5r7g3v5x-3001.app.github.dev",
+      ],
+    },
+  },
+};
 
 export default nextConfig;
